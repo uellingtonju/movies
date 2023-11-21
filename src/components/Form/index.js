a30c13d3c07a528d277ec91d88dede0cbd3d2865
mateucmc <mateucmc@gmail.com>
2023-11-21 14:12:50 -0300

    novos commts

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -8,11 +8,38 @@ function Form() {
   const [videos, setVideos] = useState([]);
   const [errors, setErrors] = useState("");
 
+  function valideUrl(url) {
+    const regex =
+      /^(?:https?:\/\/)?(?:www\.)?(?:youtube\.com\/(?:embed\/|watch\?v=)|youtu\.be\/)([a-zA-Z0-9\-_]+)$/;
+
+    if (!regex.test(url) || url.length < 43) {
+      setErrors("ERRO: URL inválida!");
+      return false;
+    } else {
+      return url.substring(32, 43); // id do video
+    }
+  }
+
   function onSave(e) {
     e.preventDefault();
-    const newVideo = { url, category };
-    setVideos([...videos, newVideo]);
-    console.log(videos);
+
+    if (!category || category === "-") {
+      setErrors("ERRO: Escolha uma categoria!");
+      return;
+    } else {
+      setErrors("");
+    }
+
+    const urlVideo = valideUrl(url);
+    if (urlVideo && category) {
+      const newVideo = { url, category };
+      setVideos([...videos, newVideo]);
+      localStorage.setItem("videos", JSON.stringify([...videos, newVideo]));
+      setUrl("");
+      setCategory("");
+    } else {
+      setErrors("ERRO: url iválida");
+    }
   }
 
   return (
@@ -28,6 +55,8 @@ function Form() {
               required="required"
               value={url}
               onChange={(e) => setUrl(e.target.value)}
+              minLength="43"
+              maxLength="43"
             />
           </div>
           <div>
@@ -46,6 +75,7 @@ function Form() {
           <div>
             <button>Cadastrar</button>
           </div>
+          <div>{errors}</div>
         </form>
       </section>
     </div>
